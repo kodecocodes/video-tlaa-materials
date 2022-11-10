d17f800188bef3f1493ec6d9df510b948c0dcc37
aldo <aldo2122@outlook.com>
2022-11-09 19:43:24 -0600

    adds folders and project

---
+++ 09-write-your-passing-test/final/app/src/androidTest/java/com/raywenderlich/codingcompanionfinder/SimpleIdlingResource.kt
@@ -0,0 +1,38 @@
+package com.raywenderlich.codingcompanionfinder
+
+import androidx.annotation.Nullable
+import androidx.test.espresso.IdlingResource
+import java.util.concurrent.atomic.AtomicInteger
+
+
+class SimpleIdlingResource : IdlingResource {
+
+    @Nullable
+    @Volatile
+    private var callback: IdlingResource.ResourceCallback? = null
+
+    // Idleness is controlled with this boolean.
+    var activeResources = AtomicInteger(0)
+
+    override fun getName(): String {
+        return this.javaClass.name
+    }
+
+    override fun isIdleNow(): Boolean {
+        return activeResources.toInt() < 1
+    }
+
+    override fun registerIdleTransitionCallback(
+        callback: IdlingResource.ResourceCallback
+    ) {
+        this.callback = callback
+    }
+
+    fun incrementBy(incrementValue: Int) {
+        if (activeResources.addAndGet(incrementValue) < 1 &&
+            callback != null
+        ) {
+            callback!!.onTransitionToIdle()
+        }
+    }
+}
---
+++ 09-write-your-passing-test/final/app/src/main/java/com/raywenderlich/codingcompanionfinder/testhooks/IdlingEntity.kt
@@ -0,0 +1,6 @@
+package com.raywenderlich.codingcompanionfinder.testhooks
+
+data class IdlingEntity(
+    var incrementValue: Int = 0,
+    var resetValue: Boolean = false
+)
